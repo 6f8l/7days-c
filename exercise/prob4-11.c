2525edcee028ecd6368c6b7c40941c4a3d6c1911
6f8l <atsuki02jojo@gmail.com>
2020-05-11 18:27:11 +0900

    Add prob4-12

--- exercise/prob4-11.c
+++ exercise/prob4-11.c
@@ -4,11 +4,16 @@
 #include <time.h>
 
 void main() {
-  int i, num;
+  int i, num, max_i;
   srand((unsigned)time(NULL));
 
+  max_i = 0;
   for (i = 0; i < 5; i++) {
     num = rand() % 100 + 1;
     printf("%d\n", num);
+    if (num > max_i) {
+      max_i = num;
+    }
   }
+  printf("%d\n", max_i);
 }
--- exercise/prob4-12.c
+++ exercise/prob4-12.c
@@ -4,11 +4,16 @@
 #include <time.h>
 
 void main() {
-  int i, num;
+  int i, num, min_i;
   srand((unsigned)time(NULL));
 
+  min_i = 100;
   for (i = 0; i < 5; i++) {
     num = rand() % 100 + 1;
     printf("%d\n", num);
+    if (num < min_i) {
+      min_i = num;
+    }
   }
+  printf("最小値: %d\n", min_i);
 }
