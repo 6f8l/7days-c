670a5e6fdd8fc4a4949cb70609e0da2b52f7f50d
6f8l <atsuki02jojo@gmail.com>
2020-05-12 17:00:04 +0900

    Add prob5-10

--- exercise/prob5/prob5-10.c
+++ exercise/prob5/prob5-10.c
@@ -5,29 +5,20 @@
 
 void main() {
   int a[10];
-  int i, num;
+  int i, j, num;
   srand((unsigned)time(NULL));
 
   for (i = 0; i < 10; i++) {
-    num = rand() % 100;
+    num = rand() % 10 + 1;
     a[i] = num;
     printf("%d ", a[i]);
   }
   printf("\n");
 
-  printf("50以上の数: ");
   for (i = 0; i < 10; i++) {
-    if (a[i] >= 50) {
-      printf("%d ", a[i]);
+    for (j = 0; j < a[i]; j++) {
+      printf("* ");
     }
+    printf("\n");
   }
-  printf("\n");
-
-  printf("50未満の数: ");
-  for (i = 0; i < 10; i++) {
-    if (a[i] < 50) {
-      printf("%d ", a[i]);
-    }
-  }
-  printf("\n");
 }
