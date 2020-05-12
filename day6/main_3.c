338a5d7eb271bdb5a8701bb1fdd19d508acf6e53
6f8l <atsuki02jojo@gmail.com>
2020-05-12 17:25:24 +0900

    Add main3

--- day6/main_3.c
+++ day6/main_3.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-double avg(double l, double m) {
-  double r = (l + m) / 2.0;
-  return r;
-}
+double avg(double, double);
 
 void main() {
   double d1, d2, d3;
@@ -12,4 +9,9 @@ void main() {
   d2 = avg(4.1, 5.7);
   d3 = avg(c, 2.8);
   printf("d1=%f, d2=%f, d3=%f\n", d1, d2, d3);
+}
+
+double avg(double l, double m) {
+  double r = (l + m) / 2.0;
+  return r;
 }
