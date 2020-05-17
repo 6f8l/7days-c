9efd3cf1a5ba922509654777fc547f2b498d503c
6f8l <atsuki02jojo@gmail.com>
2020-05-17 17:08:47 +0900

    Add main2

--- day1/advanced/main_2.c
+++ day1/advanced/main_2.c
@@ -1,7 +1,15 @@
+#include <math.h>
 #include <stdio.h>
 
+#define PI 3.14
+
 void main() {
-  printf("123\n456\n789\n");
-  printf("シングルクオーテーション：\'ダブルクオーテーション：\"\n");
-  printf("\t円マーク\\\n");
+  int angle;
+  double rad;
+  printf("角度を入力してください(0～360):");
+  scanf("%d", &angle);
+  rad = PI * (double)angle / 180.0;
+  printf("sin(%d)=%f\n", angle, sin(rad));
+  printf("cos(%d)=%f\n", angle, cos(rad));
+  printf("tan(%d)=%f\n", angle, tan(rad));
 }
