56365441a0918696a2f99ebcf1119d5b935989e7
6f8l <atsuki02jojo@gmail.com>
2020-05-17 17:17:43 +0900

    Add main4

--- day1/advanced/main_4.c
+++ day1/advanced/main_4.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main() {
-  printf("ABC");
-  printf("DEF\n");
+  unsigned char i = 0xf;
+  unsigned char j = 0xff;
+
+  printf("%x << 1 = %x\n", i, i << 1);
+  printf("%x >> 1 = %x\n", i, i >> 1);
+  printf("%x | %x = %x\n", i, j, i | j);
+  printf("%x & %x = %x\n", i, j, i & j);
+  printf("~%x = %x\n", i, (unsigned char)~i);
 }
