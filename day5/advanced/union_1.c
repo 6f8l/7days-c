fcc5c998172bbe555616a17d513669023ac3b7e5
6f8l <atsuki02jojo@gmail.com>
2020-05-21 23:52:23 +0900

    Add union

---
+++ day5/advanced/union_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+union data {
+  int n;
+  double d;
+  char s[16];
+};
+
+int main() {
+  union data d[3];
+  d[0].n = 1;
+  d[1].d = 3.14;
+  strcpy(d[2].s, "Hello");
+  printf("%d %f %s\n", d[0].n, d[1].d, d[2].s);
+}
